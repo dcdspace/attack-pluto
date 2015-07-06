7614b1bbac5face4b9a927b5b176a85dc1930d2b
Dylan Diamond <dylan@dcdwebdesign.com>
2015-07-06 11:04:21 -0700

    d

---
+++ entities.js
@@ -0,0 +1,108 @@
+var PlayerEntity = me.ObjectEntity.extend({
+  init: function(x, y, settings) {
+    this.parent(x, y, settings);
+    me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
+    this.setVelocity(3, 12);
+  },
+  update: function() {
+    if (me.input.isKeyPressed('left')) { this.move(true); }
+    else if (me.input.isKeyPressed('right')) { this.move(false); }
+    else { this.vel.x = 0; };
+    if (me.input.isKeyPressed('jump')) { this.doJump(); }
+    me.game.collide(this);
+    this.updateMovement();
+    if (this.bottom > 490){ this.gameOver(); }
+    if (this.vel.x!=0 || this.vel.y!=0) {
+      this.parent(this);
+      return true;
+    }
+    return false;
+  },
+  gameOver: function() {
+    me.state.change(me.state.MENU);
+  },
+  nextLevel: function () {
+    me.gamestat.updateValue("currentLevel", 1);
+    me.levelDirector.loadLevel("level2");
+    me.gamestat.updateValue("coins", 0);
+    me.gamestat.updateValue("totalCoins", 3);
+
+  },
+  youWin: function() {
+    me.state.change(me.state.MENU);
+    document.getElementById('game_state').innerHTML = "You Win!";
+    document.getElementById('instructions').innerHTML = "";
+  },
+  move: function(moveLeft) {
+    this.flipX(moveLeft);
+    this.vel.x += moveLeft ? -0.1 : 0.1;
+  }
+});
+
+var RocketEntity = me.ObjectEntity.extend( {
+  init: function(x, y, settings) {
+    this.parent(x, y, settings);
+  }
+});
+var CoinEntity = me.CollectableEntity.extend({
+  init: function(x, y, settings) {
+    this.parent(x, y, settings);
+  },
+  onCollision : function (res, obj) {
+    me.gamestat.updateValue("coins", 1);
+    this.collidable = false;
+    me.game.remove(this);
+    if(me.gamestat.getItemValue("coins") === me.gamestat.getItemValue("totalCoins")){
+      if (me.gamestat.getItemValue("currentLevel") == 2) {
+        obj.gameOver();
+      }
+      else
+      {
+        obj.nextLevel();
+      }
+    }
+  }
+}); 
+var EnemyEntity = me.ObjectEntity.extend({
+  init: function(x, y, settings) {
+    settings.image = "badguy";
+    settings.spritewidth = 16;
+    this.parent(x, y, settings);
+    this.startX = x;
+    this.endX = x + settings.width - settings.spritewidth;
+    this.pos.x = this.endX;
+    this.walkLeft = true;
+    this.setVelocity(2);
+    this.collidable = true;
+  },
+  onCollision: function(res, obj) {
+    console.log("player: " + obj.bottom + "snake: " + this.bottom);
+    if (obj.bottom < this.bottom) {
+     me.game.remove(this);
+    }
+    else {
+      obj.gameOver();
+    }
+  },
+  update: function() {
+    if (!this.visible){
+      return true;
+    }
+    if (this.alive) {
+      if (this.walkLeft && this.pos.x <= this.startX) {
+        this.walkLeft = false;
+      }
+      else if (!this.walkLeft && this.pos.x >= this.endX){ 
+        this.walkLeft = true;
+      }
+      this.doWalk(this.walkLeft);
+    }
+    else { this.vel.x = 0; }
+    this.updateMovement();
+    if (this.vel.x!=0 || this.vel.y!=0) {
+      this.parent(this);
+      return true;
+    }
+    return false;
+  }
+});
---
+++ resources.js
@@ -0,0 +1,50 @@
+var resources = [
+  {
+    name: "level1",
+    type: "tmx",
+    src: "level1.tmx"
+  },
+  {
+    name: "player",
+    type: "image",
+    src: "player.png"
+  },
+  { name: "titleScreen",
+    type: "image",
+    src: "titleScreen.png"
+  },
+  { name: "coin",
+    type: "image",
+    src: "coin.png"
+  },
+  {
+    name: "rocket",
+    type: "image",
+    src: "rocket.png"
+  },
+  { name: "badGuy",
+    type: "image",
+    src: "badGuy.png"
+  },
+  { name: "badGuy",
+    type: "image",
+    src: "badGuy.png"
+  },
+  { name: "ice",
+    type: "image",
+    src: "ice.jpg"
+  },
+  { name: "rocket",
+    type: "image",
+    src: "rocket.png"
+  },
+  { name: "sky",
+    type: "image",
+    src: "sky.gif"
+  },
+  { name: "pluto",
+    type: "image",
+    src: "pluto.jpg"
+  },
+
+];
